b81773f923c2a146d94fcde7110cbabe7d382cee
HenBenjamin <henbenjamin2004@gmail.com>
2026-02-09 17:03:09 +0100

    Biztonsagi frissites

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 const PARAMS = [
     "HOST" => 'localhost',
     "USER" => 'hh',
-    "PASSWORD" => 'mjyDzHfqN9vsDVA',
+    "PASSWORD" => '',
     "DB" => 'hh',
     "CHARSET" => 'utf8mb4'
 ];
